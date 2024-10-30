9fa4f072d436b68c70bfcf130108fc3f024bc91d
sast-qa <123078419+sast-qa@users.noreply.github.com>
2024-10-30 09:41:56 +0100

    Add/Update bad/libuser.py file to repo.

--- bad/libuser.py
+++ bad/libuser.py
@@ -1,61 +1,29 @@
-import sqlite3
-import libuser
+#!/usr/bin/env python3
+
+from flask import Flask, g, redirect, request
+
+from mod_hello import mod_hello
+from mod_user import mod_user
+from mod_posts import mod_posts
+from mod_mfa import mod_mfa
+
+import libsession
+
+app = Flask('vulpy')
+app.config['SECRET_KEY'] = 'aaaaaaa'
+
+app.register_blueprint(mod_hello, url_prefix='/hello')
+app.register_blueprint(mod_user, url_prefix='/user')
+app.register_blueprint(mod_posts, url_prefix='/posts')
+app.register_blueprint(mod_mfa, url_prefix='/mfa')
 
 
-def login(username, password):
+@app.route('/')
+def do_home():
+    return redirect('/posts')
 
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
+@app.before_request
+def before_request():
+    g.session = libsession.load(request)
 
-    user = c.execute("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(username, password)).fetchone()
-
-    if user:
-        return user['username']
-    else:
-        return False
-
-
-def create(username, password):
-
-    conn = sqlite3.connect('db_users.sqlite')
-    c = conn.cursor()
-
-    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
-
-    conn.commit()
-    conn.close()
-
-
-def userlist():
-
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    users = c.execute("SELECT * FROM users").fetchall()
-
-    if not users:
-        return []
-    else:
-        return [ user['username'] for user in users ]
-
-
-def password_change(username, password):
-
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
-    conn.commit()
-
-    return True
-
-
-def password_complexity(password):
-    return True
-
+app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
